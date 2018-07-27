e2317b52ff9840c840b00cbac05508b1441c0c8e
Dhananjay <dhananjaymohnot@gmail.com>
2018-07-27 12:59:47 +0530

    show application versionCode.

--- app/src/main/java/uncaught/exception/handler/sample/android/ExceptionHandler.java
+++ app/src/main/java/uncaught/exception/handler/sample/android/ExceptionHandler.java
@@ -2,23 +2,25 @@ package uncaught.exception.handler.sample.android;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 class ExceptionHandler implements Thread.UncaughtExceptionHandler {
-    private Context activity;
+    private Context context;
+    private String LINE_SEPARATOR = "\n";
 
     ExceptionHandler(Context context) {
-        this.activity = context;
+        this.context = context;
     }
 
     @Override
     public void uncaughtException(Thread thread, Throwable exception) {
         StringWriter stackTrace = new StringWriter();
         exception.printStackTrace(new PrintWriter(stackTrace));
-        String LINE_SEPARATOR = "\n";
         String errorReport = "***********CAUSE OF ERROR***********\n\n" +
                 stackTrace.toString() +
                 "\n***********DEVICE INFORMATION***********\n\n" +
@@ -46,13 +48,30 @@ class ExceptionHandler implements Thread.UncaughtExceptionHandler {
                 LINE_SEPARATOR +
                 "INCREMENTAL" +
                 Build.VERSION.INCREMENTAL +
-                LINE_SEPARATOR;
+                LINE_SEPARATOR +
+                "\n***********Application Info***********\n\n"
+                + fetchCurrentApplicationInfo();
 
-        Intent intent = new Intent(activity, MainActivity.class);
+        Intent intent = new Intent(context, MainActivity.class);
         intent.putExtra("error", errorReport);
-        activity.startActivity(intent);
+        context.startActivity(intent);
 
         android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(10);
+    }
+
+    private String fetchCurrentApplicationInfo() {
+        long longVersionCode = 0;
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            if (Build.VERSION.SDK_INT >= 28) {
+                longVersionCode = packageInfo.getLongVersionCode();
+            } else {
+                longVersionCode = packageInfo.versionCode;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "longVersionCode:" + longVersionCode + LINE_SEPARATOR;
     }
 }
